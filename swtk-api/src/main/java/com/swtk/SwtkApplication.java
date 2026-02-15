52d56a9f903797f494338719512eae87a3f2f0a5
soumesh2002 <soumesh6622@gmail.com>
2026-02-16 00:18:47 +0530

    added init setup: backend and frontend

---
+++ swtk-api/src/main/java/com/swtk/SwtkApplication.java
@@ -0,0 +1,13 @@
+package com.swtk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwtkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwtkApplication.class, args);
+	}
+
+}
---
+++ swtk-api/src/test/java/com/swtk/SwtkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swtk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwtkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
